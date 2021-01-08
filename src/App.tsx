cd1b5fd983d3454ce551034f485117898feb882e
孤独な 宇宙飛行士 <vadim1230981@gmail.com>
2021-01-08 17:52:56 +0300

    Fix linter

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,6 @@ import {
     ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-declare const global: { HermesInternal: null | {} };
-
 const App = () => {
     return (
         <>
@@ -27,11 +25,6 @@ const App = () => {
             <SafeAreaView>
                 <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
                     <Header />
-                    {global.HermesInternal == null ? null : (
-                        <View style={styles.engine}>
-                            <Text style={styles.footer}>Engine: Hermes</Text>
-                        </View>
-                    )}
                     <View style={styles.body}>
                         <View style={styles.sectionContainer}>
                             <Text style={styles.sectionTitle}>Step One</Text>
