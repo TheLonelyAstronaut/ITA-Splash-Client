01b1655ec52630877e7bba2169a85964e7d10be4
孤独な 宇宙飛行士 <vadim1230981@gmail.com>
2021-01-24 23:11:22 +0300

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,20 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { RootRouterComponent } from './app/routing/root-router.component';
 import { useStore } from './app/store/use-store';
 import { ConnectedThemeProvider } from './app/ui/connected-theme-provider.component';
+import RNTrackPlayer, { Event } from 'react-native-track-player';
 
 export const App: React.FC = () => {
     const store = useStore();
+
+    React.useEffect(() => {
+        if (store) {
+            // Add listener to change store
+            RNTrackPlayer.addEventListener(Event.PlaybackTrackChanged, () => {
+                // store.store.dispatch()
+            });
+        }
+    }, [store]);
+
     if (!store) {
         return null;
     }
