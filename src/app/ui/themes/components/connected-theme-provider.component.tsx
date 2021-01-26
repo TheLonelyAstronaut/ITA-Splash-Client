7ca9e433179c4f923cb5dbe606b6f9ab8d3d7c56
孤独な 宇宙飛行士 <vadim1230981@gmail.com>
2021-01-26 14:17:47 +0300

    Colorizing android nav- and status- bar.

--- src/app/ui/themes/components/connected-theme-provider.component.tsx
+++ src/app/ui/themes/components/connected-theme-provider.component.tsx
@@ -1,11 +1,13 @@
 import React, { ReactNode } from 'react';
-import { StatusBar } from 'react-native';
+import { Platform, StatusBar } from 'react-native';
 import { useSelector } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { DefaultTheme } from 'styled-components/native';
 
 import { getTheme } from '../selectors';
-import { themesCollection, ThemesEnum } from '../themes';
+import { darkTheme, lightTheme, themesCollection, ThemesEnum } from '../themes';
+
+import changeNavigationBarColor from 'react-native-navigation-bar-color';
 
 type Props = {
     children: ReactNode;
@@ -15,8 +17,21 @@ export const ConnectedThemeProvider: React.FC<Props> = (props: Props) => {
     const theme = useSelector(getTheme);
 
     const barStyle = React.useMemo(() => {
-        if (theme === ThemesEnum.DARK) return 'light-content';
-        else return 'dark-content';
+        if (theme === ThemesEnum.DARK) {
+            if (Platform.OS === 'android') {
+                StatusBar.setBackgroundColor(darkTheme.colors.screenBackground, true);
+                changeNavigationBarColor(darkTheme.colors.main, false, true);
+            }
+
+            return 'light-content';
+        } else {
+            if (Platform.OS === 'android') {
+                StatusBar.setBackgroundColor(lightTheme.colors.screenBackground, true);
+                changeNavigationBarColor(lightTheme.colors.main, true, true);
+            }
+
+            return 'dark-content';
+        }
     }, [theme]);
 
     return (
