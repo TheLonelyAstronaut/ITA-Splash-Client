af5056943472160e180e28e76a21c95f0e26e1b0
孤独な 宇宙飛行士 <vadim1230981@gmail.com>
2021-02-09 02:20:19 +0300

    Some stack animation improvements

--- src/app/routing/components/root-router.component.tsx
+++ src/app/routing/components/root-router.component.tsx
@@ -1,4 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { useSelector } from 'react-redux';
 
@@ -7,7 +8,19 @@ import { getUsername } from '../../authentication/selectors';
 
 import { MainStackComponent } from './main-stack.component';
 
+const AppStack = createStackNavigator();
+
 export const RootRouterComponent: React.FC = () => {
     const isLoggedIn = useSelector(getUsername);
-    return <NavigationContainer>{isLoggedIn ? <MainStackComponent /> : <AuthStack />}</NavigationContainer>;
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator headerMode={'none'} mode={'modal'}>
+                {isLoggedIn ? (
+                    <AppStack.Screen component={MainStackComponent} name={'MainModule'} />
+                ) : (
+                    <AppStack.Screen component={AuthStack} name={'AuthModule'} />
+                )}
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
 };
