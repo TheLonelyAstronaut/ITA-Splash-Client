a193aa725db24f375cfb4736634f47b55cd7a299
孤独な 宇宙飛行士 <vadim1230981@gmail.com>
2021-03-16 14:53:28 +0300

    Changing color of navbar while splashscreen hiding

--- src/app/ui/themes/components/connected-theme-provider.component.tsx
+++ src/app/ui/themes/components/connected-theme-provider.component.tsx
@@ -1,12 +1,11 @@
 import React, { ReactNode } from 'react';
-import { Platform, StatusBar } from 'react-native';
-import changeNavigationBarColor from 'react-native-navigation-bar-color';
+import { StatusBar } from 'react-native';
 import { useSelector } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { DefaultTheme } from 'styled-components/native';
 
 import { getTheme } from '../selectors';
-import { darkTheme, lightTheme, themesCollection, ThemesEnum } from '../themes';
+import { themesCollection, ThemesEnum } from '../themes';
 
 type Props = {
     children: ReactNode;
@@ -16,21 +15,8 @@ export const ConnectedThemeProvider: React.FC<Props> = (props: Props) => {
     const theme = useSelector(getTheme);
 
     const barStyle = React.useMemo(() => {
-        if (theme === ThemesEnum.DARK) {
-            if (Platform.OS === 'android') {
-                StatusBar.setBackgroundColor(darkTheme.colors.screenBackground, true);
-                changeNavigationBarColor(darkTheme.colors.main, false, true);
-            }
-
-            return 'light-content';
-        } else {
-            if (Platform.OS === 'android') {
-                StatusBar.setBackgroundColor(lightTheme.colors.screenBackground, true);
-                changeNavigationBarColor(lightTheme.colors.main, true, true);
-            }
-
-            return 'dark-content';
-        }
+        if (theme === ThemesEnum.DARK) return 'light-content';
+        else return 'dark-content';
     }, [theme]);
 
     return (
